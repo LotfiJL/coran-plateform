14b418e63951145ce01682d422f306fceb41668e
Lotfi Jellali <lotfijellali40@gmail.com>
2024-02-22 01:17:52 +0100

    Hls video player

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,3 +89,15 @@ function PlyerSuraa(surahMp3) {
   audioPlayer.src = surahMp3;
   audioPlayer.play();
 }
+
+function playLive(channel) {
+  if (Hls.isSupported()) {
+    var video = document.getElementById("Lvideo");
+    var hls = new Hls();
+    hls.loadSource(`${channel}`);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, function () {
+      video.play();
+    });
+  }
+}
